49aa07654ac34afafe74ce551b6bd59bc2dcfae5
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-15 00:09:30 -0300

    Update questão1.sql

--- questão1.sql
+++ questão1.sql
@@ -1,2 +1,8 @@
-CREATE OR REPLACE FUNCTION idade_func() RETURNS INT
-RETURN
+DELIMITER $
+CREATE OR REPLACE FUNCTION calc_idade_func(INT codigo) RETURNS INT
+BEGIN
+SET @idade := (SELECT DATEDIFF(MONTH,'dataNasc',GETDATE())/12 WHERE 
+Funcionario.codigo = codigo);
+RETURN @idade;
+END $
+DELIMITER;
