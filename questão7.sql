586be431b978c8a70676d1f7fdf810655a5ba8a4
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 20:57:26 -0300

    Update questão7.sql

--- questão7.sql
+++ questão7.sql
@@ -1,1 +1,12 @@
-
+CREATE OR REPLACE FUNCTION total_ativ_membro(codProjeto INTEGER)
+RETURNS INTEGER
+AS $$
+DECLARE
+total_atividades INTEGER;
+BEGIN
+SELECT INTO total_atividades count(*)
+FROM funcionario f, atividade_membro am
+WHERE am.codmembro=f.codigo;
+RETURN total_atividades;
+END;
+$$ LANGUAGE 'plpgsql';
