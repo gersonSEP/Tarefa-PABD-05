fbc08172ac041f3e8153501e911fca6897e6d384
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 20:43:49 -0300

    Update questão4.sql

--- questão4.sql
+++ questão4.sql
@@ -1,1 +1,18 @@
+CREATE OR REPLACE FUNCTION atv_atrasada(codAtividade INTEGER)
+RETURNS INTERVAL
+AS $$
+DECLARE
+fim DATE;
+hoje timestamp;
+diasAtraso INTERVAL;
+BEGIN
+SELECT INTO hoje NOW();
+SELECT INTO fim datafim
+FROM atividade
+WHERE codigo=codAtividade;
+diasAtraso=fim-hoje;
+return diasAtraso;
+END;
+$$ LANGUAGE 'plpgsql';
+
 
