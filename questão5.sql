0256694e76814f387851e154d7d2b119150b283f
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 20:53:25 -0300

    Update questão5.sql

--- questão5.sql
+++ questão5.sql
@@ -1,1 +1,14 @@
-
+CREATE OR REPLACE FUNCTION proj_atrasado(codProjeto INT)
+RETURNS INT
+DECLARE
+fim DATE;
+hoje timestamp;
+diasAtraso INT;
+BEGIN
+SELECT INTO hoje NOW();
+SELECT INTO fim datafim
+FROM projeto
+WHERE codigo=codProjeto;
+diasAtraso=fim-hoje;
+RETURN diasAtraso;
+END;
