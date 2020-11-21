a4f069d1ac759ef2150622a9c1c7d8da85bf5b3f
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 20:54:51 -0300

    Update questão6.sql

--- questão6.sql
+++ questão6.sql
@@ -1,1 +1,10 @@
-
+CREATE OR REPLACE PROCEDURE equipe_proj()
+AS $$
+BEGIN
+SELECT e.nomeequipe, f.nome, d.sigla
+FROM projeto proj, equipe e, departamento d, funcionario f
+WHERE proj.equipe=e.codigo
+AND d.codigo=proj.depto
+AND f.codigo=proj.responsavel;
+END;
+$$ LANGUAGE plpgsql;
