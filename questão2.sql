342253d4f9765b2ea494e74744e8bcaf96ecdc0f
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 20:38:43 -0300

    Update questão2.sql

--- questão2.sql
+++ questão2.sql
@@ -1,1 +1,13 @@
 
+DELIMITER $
+CREATE OR REPLACE FUNCTION idade_media_depto(depto INT)
+RETURNS INT
+BEGIN
+SELECT INTO @media AVG(EXTRACT(years FROM AGE(f.datanasc)))
+FROM funcionario f
+WHERE f.depto = depto;
+RETURN @media;
+END $
+DELIMITER $
+
+
