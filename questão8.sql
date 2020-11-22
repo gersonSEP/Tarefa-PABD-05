8d0a53a539d93fd8f03bd35eaf1987780b938648
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 21:02:19 -0300

    Update questão8.sql

--- questão8.sql
+++ questão8.sql
@@ -1,1 +1,19 @@
+CREATE OR REPLACE FUNCTION public.quant_ativ_projeto(cod_proj INTEGER)
+RETURNS INTEGER
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+total INTEGER;
+BEGIN
+SELECT INTO total count(*)
+AS total_atividades
+FROM projeto proj, atividade_projeto atv
+WHERE atv.codprojeto=proj.codigo
+AND proj.codigo=cod_proj;
+RETURN total;
+END;
+$BODY$;
 
+SELECT total_ativ_membro(proj.codigo)/quant_ativ_projeto(proj.codigo)
+AS porcentagem
+FROM projeto proj;
