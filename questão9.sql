332ef1dfc5a3446f361fd6e89ec29a16af6616a4
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 21:04:07 -0300

    Update questão9.sql

--- questão9.sql
+++ questão9.sql
@@ -1,1 +1,15 @@
+CREATE OR REPLACE
+VIEW gerentes
+AS
+SELECT f.nome
+AS nome_gerente, d.codigo
+AS dep_gerente, f.salario
+AS salario_gerente
+FROM departamento d, funcionario f
+WHERE d.gerente=f.codigo;
 
+SELECT f.nome, d.sigla
+FROM funcionario f, departamento d, gerentes ge
+WHERE f.salario>ge.salario_gerente
+AND d.codigo=ge.dep_gerente
+AND f.depto=d.codigo;
